2ff794b58eda83d58d5d551ac4699845007fd8e5
Christian Uy <tr1b1iwnl@gmail.com>
2024-12-11 04:55:14 +0800

    Debug environment

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+console.log("Environment:", process.env.NODE_ENV);
+console.log(
+  "API Base URL:",
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_BOARDPREP_API
+    : "http://20.189.73.26:8000"
+);
+
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
