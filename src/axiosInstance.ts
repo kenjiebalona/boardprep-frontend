30fff4d9690fe44ec0eaf833b5b5f8c209881530
Christian Uy <tr1b1iwnl@gmail.com>
2024-12-11 05:53:40 +0800

    Changed backend url

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,18 +1,7 @@
 import axios from "axios";
 
-console.log("Environment:", process.env.NODE_ENV);
-console.log(
-  "API Base URL:",
-  process.env.NODE_ENV === "production"
-    ? process.env.VITE_BOARDPREP_API
-    : "http://20.189.73.26:8000"
-);
-
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? process.env.VITE_BOARDPREP_API
-      : "http://20.189.73.26:8000",
+  baseURL: "http://20.189.73.26:8000",
   withCredentials: true,
 });
 
