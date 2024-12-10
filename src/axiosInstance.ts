47ed63cca130e3409ff632525808b861336e2a0b
Christian Uy <tr1b1iwnl@gmail.com>
2024-12-11 05:47:59 +0800

    Changed react app to vite

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -4,14 +4,14 @@ console.log("Environment:", process.env.NODE_ENV);
 console.log(
   "API Base URL:",
   process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_BOARDPREP_API
+    ? process.env.VITE_BOARDPREP_API
     : "http://20.189.73.26:8000"
 );
 
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_BOARDPREP_API
+      ? process.env.VITE_BOARDPREP_API
       : "http://20.189.73.26:8000",
   withCredentials: true,
 });
